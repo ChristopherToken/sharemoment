db50fb9b81fb9748edb13fe2c50f69e9d51c2d92
ChristopherToken <39127305+ChristopherToken@users.noreply.github.com>
2018-09-13 19:23:19 +0700

    Add files via upload

---
+++ gallery-feed.php
@@ -0,0 +1,237 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Kanit');
+    </style>
+    <link type="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
+    <title>Responsive Event Html template bootstrap 4 - Bootsnipp.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+    </style>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+
+    <style>
+    
+    .thumbnail {
+    display: block;
+    padding: 4px;
+    margin-bottom: 0px;
+    line-height: 1.42857143;
+    background-color: #fff;
+    border: none;
+    border-radius: 4px;
+    -webkit-transition: all .1s ease-in-out;
+    -o-transition: all .1s ease-in-out;
+    transition: all .1s ease-in-out;
+}
+
+
+    </style>
+</head>
+<body>
+<div class="container">
+
+                <div class="col-md-12">
+                    <div id="down" class="row">
+                        <hr>
+
+                        <div class="gal">
+
+                        
+                   <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"   data-caption="Some lovely red flowers" data-caption="Some lovely red flowers" data-image="https://images.unsplash.com/photo-1532370184535-22cec5ca8480?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c0af28a51f414df550baa22cc4dec28f&auto=format&fit=crop&w=1350&q=80" data-target="#image-gallery">
+                        <img class="img-responsive" src="https://images.unsplash.com/photo-1532370184535-22cec5ca8480?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c0af28a51f414df550baa22cc4dec28f&auto=format&fit=crop&w=1350&q=80" alt="">
+                   </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.unsplash.com/photo-1523044832861-02368d48d715?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=003266d0f63b9098b970a00c0028b486&auto=format&fit=crop&w=634&q=80" data-target="#image-gallery" >
+                        <img src="https://images.unsplash.com/photo-1523044832861-02368d48d715?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=003266d0f63b9098b970a00c0028b486&auto=format&fit=crop&w=634&q=80" class="img-fluid" alt="Responsive image">
+                    </a>                
+                        
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/904911/pexels-photo-904911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >
+                        <img src="https://images.pexels.com/photos/904911/pexels-photo-904911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.unsplash.com/photo-1505253668822-42074d58a7c6?ixlib=rb-0.3.5&s=ac1ee4a26a037bfdd8a78fda77c496f4&auto=format&fit=crop&w=634&q=80" data-target="#image-gallery" >
+                        <img src="https://images.unsplash.com/photo-1505253668822-42074d58a7c6?ixlib=rb-0.3.5&s=ac1ee4a26a037bfdd8a78fda77c496f4&auto=format&fit=crop&w=634&q=80" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.unsplash.com/photo-1521566878949-d0febfb1e9bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e3b5ee0942ce548abde8fbd1cb096330&auto=format&fit=crop&w=1974&q=80" data-target="#image-gallery" >
+                        <img src="https://images.unsplash.com/photo-1521566878949-d0febfb1e9bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e3b5ee0942ce548abde8fbd1cb096330&auto=format&fit=crop&w=1974&q=80" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.unsplash.com/photo-1519549735396-e5f6f8ee578c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35d25dac36ac5de19838c2e22755d0b8&auto=format&fit=crop&w=1350&q=80" data-target="#image-gallery" >
+                            <img src="https://images.unsplash.com/photo-1519549735396-e5f6f8ee578c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35d25dac36ac5de19838c2e22755d0b8&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </a> 
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.unsplash.com/photo-1515233496779-1fe0eedba426?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d6b5dbd94efc099e0a08e8dba05bf423&auto=format&fit=crop&w=1350&q=80" data-target="#image-gallery" >                    
+                            <img src="https://images.unsplash.com/photo-1515233496779-1fe0eedba426?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d6b5dbd94efc099e0a08e8dba05bf423&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </a>
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/518213/pexels-photo-518213.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                    
+                        <img src="https://images.pexels.com/photos/518213/pexels-photo-518213.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a> 
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.unsplash.com/photo-1507358522600-9f71e620c44e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9606de2cffd6c619093871ef2d1c0e6f&auto=format&fit=crop&w=1350&q=80" data-target="#image-gallery" >                    
+                        <img src="https://images.unsplash.com/photo-1507358522600-9f71e620c44e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9606de2cffd6c619093871ef2d1c0e6f&auto=format&fit=crop&w=1350&q=80" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/460376/pexels-photo-460376.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                        
+                        <img src="https://images.pexels.com/photos/460376/pexels-photo-460376.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/721169/pexels-photo-721169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                            
+                            <img src="https://images.pexels.com/photos/721169/pexels-photo-721169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/460223/pexels-photo-460223.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                            
+                         <img src="https://images.pexels.com/photos/460223/pexels-photo-460223.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/1376967/pexels-photo-1376967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                            
+                        <img src="https://images.pexels.com/photos/1376967/pexels-photo-1376967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/730426/pexels-photo-730426.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                
+                         <img src="https://images.pexels.com/photos/730426/pexels-photo-730426.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/733035/pexels-photo-733035.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                
+                        <img src="https://images.pexels.com/photos/733035/pexels-photo-733035.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/161197/wat-phra-temple-chiang-mai-thailand-161197.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                
+                         <img src="https://images.pexels.com/photos/161197/wat-phra-temple-chiang-mai-thailand-161197.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/733030/pexels-photo-733030.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                
+                            <img src="https://images.pexels.com/photos/733030/pexels-photo-733030.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>  
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/730427/pexels-photo-730427.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                
+                            <img src="https://images.pexels.com/photos/730427/pexels-photo-730427.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+                    
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/257433/pexels-photo-257433.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                                    
+                            <img src="https://images.pexels.com/photos/257433/pexels-photo-257433.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                    <a class="thumbnail" href="#" data-image-id="" data-toggle="modal"  data-caption="Thai tample please pray for Thailand Some lovely red flowers" data-image="https://images.pexels.com/photos/234757/pexels-photo-234757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" data-target="#image-gallery" >                                                                                                    
+                        <img src="https://images.pexels.com/photos/234757/pexels-photo-234757.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                    </a>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md" >
+            <div class="modal-content">
+                <div class="modal-header">
+                <div class="col-8">
+                        <span class="photoBy">
+                       <p>Shoot by: <a class="photoBy" id="BY" href="">Komsak</a></p>
+                        </span>
+                    </div>
+                    <div class="col-md-4">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <img id="image-gallery-image" class="img-responsive img-fluid"  src="">
+                </div>
+                <div class="modal-footer">
+                    <!--
+                    <div class="col-md-4">
+                        <button type="button" class="btn btn-primary" id="show-previous-image">Previous</button>
+                    </div>
+                    -->
+                    
+                    <div class="col-md-8 text-justify" id="image-gallery-caption">
+                        This text will be overwritten by jQuery
+                    </div>
+                    <div class="col-md-4">
+                    <a href="#" class="btn btn-info" role="button">Download</a>
+                    </div>
+                    <!--
+                    <div class="col-md-4">
+                        <button type="button" id="show-next-image" class="btn btn-default">Next</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                    -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+            loadGallery(true, 'a.thumbnail');
+
+            //This function disables buttons when needed
+            /*
+            function disableButtons(counter_max, counter_current) {
+                $('#show-previous-image, #show-next-image').show();
+                if (counter_max == counter_current) {
+                    $('#show-next-image').hide();
+                } else if (counter_current == 1) {
+                    $('#show-previous-image').hide();
+                }
+            }
+            */
+
+            /**
+             *
+             * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+             * @param setClickAttr  Sets the attribute for the click handler.
+             */
+
+            function loadGallery(setIDs, setClickAttr) {
+                var current_image,
+                    selector,
+                    counter = 0;
+
+                $('#show-next-image, #show-previous-image').click(function() {
+                    if ($(this).attr('id') == 'show-previous-image') {
+                        current_image--;
+                    } else {
+                        current_image++;
+                    }
+
+                    selector = $('[data-image-id="' + current_image + '"]');
+                    updateGallery(selector);
+                });
+
+                function updateGallery(selector) {
+                    var $sel = selector;
+                    current_image = $sel.data('image-id');
+                    $('#image-gallery-caption').text($sel.data('caption'));
+                    $('#image-gallery-title').text($sel.data('title'));
+                    $('#image-gallery-image').attr('src', $sel.data('image'));
+                    disableButtons(counter, $sel.data('image-id'));
+                }
+
+                if (setIDs == true) {
+                    $('[data-image-id]').each(function() {
+                        counter++;
+                        $(this).attr('data-image-id', counter);
+                    });
+                }
+                $(setClickAttr).on('click', function() {
+                    updateGallery($(this));
+                });
+            }
+        });
+    </script>
+</body>
+</html>
